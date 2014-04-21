951faee34d908244793d1ec3d0695f6fde7f2a16
Chris O'Dell <christine.ann.odell@gmail.com>
2014-04-21 01:23:16 +0100

    Updated version to 1.0.0

--- SoulGemNotifier.lua
+++ SoulGemNotifier.lua
@@ -7,7 +7,7 @@
 
 SoulGemNotifier = {}
 SoulGemNotifier.name = "SoulGemNotifier"
-SoulGemNotifier.version = "0.1.0"
+SoulGemNotifier.version = "1.0.0"
 SoulGemNotifier.debug = false
 
 function SoulGemNotifier.Initialise(eventCode, addOnName)
